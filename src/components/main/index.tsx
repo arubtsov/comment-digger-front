8a505ebab1fa6197e86fda17bc61782a71a8a84b
arubtsov <arseniy.rubtsov@gmail.com>
2019-12-20 00:29:58 +0300

    simple fetching

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const MainInput: React.FC<React.HTMLProps<HTMLInputElement>> = (props) => {
+    return (
+        <input type="text"
+            placeholder='Enter YouTube video URL'
+            className='col-sm-6 form-control' {...props}/>
+    )
+};
 
 const Main: React.FC = () => {
+    const [state, setState] = useState({ url: '', isLoading: false, data: null });
+    const onUrlChange = (event: React.ChangeEvent<HTMLInputElement>) => setState(prev =>({ ...prev, url: event.target.value }));
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setState(prev => ({ ...prev, isLoading: true }));
+
+        const urlObject = new URL(state.url);
+        const videoId = urlObject.searchParams.get('v');
+
+        fetch(`http://comment-digger-back.herokuapp.com/?videoId=${videoId}`)
+            .then(response => response.text())
+            .then(jobId => {
+                const interval = setInterval(() => {
+                    fetch(`http://comment-digger-back.herokuapp.com/results/${jobId}?number=${50}`)
+                        .then(response => {
+                            if (response.status === 200) {
+                                clearInterval(interval);
+
+                                response.json().then(data => setState(prev => ({ ...prev, data, isLoading: false })))
+                            }
+                        })
+                }, 5000);
+            });
+    };
+
     return (
         <main className='container-md'>
-            <form className='row justify-content-around'>
-                <input type="text" placeholder='Enter YouTube video URL' className='col-sm-6 form-control'/>
-                <button className='col-sm-2 btn btn-primary'>Dig!</button>
-            </form>
+            <form className='row justify-content-around' onSubmit={onSubmit}>
+                <MainInput value={state.url} onChange={onUrlChange}/>
+                <button className='col-sm-2 btn btn-primary' disabled={!state.url || state.isLoading}>Dig!</button>
+            </form>         
         </main>
     );
 };
